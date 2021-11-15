3b1dce264a84080136dfdf67d342faafd79edbf0
Talhaasre <talhaasre786@gmail.com>
2021-11-15 16:29:14 +0530

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,8 @@ const Navbar = () => {
         <div
           id="carouselExampleControls"
           class="carousel slide"
-          data-ride="carousel">
+          data-ride="carousel"
+        >
           <div class="carousel-inner cin">
             <div class="carousel-item active">
               <img class="w-100" src="cam2.jpg" alt="First slide" />
@@ -33,65 +34,77 @@ const Navbar = () => {
                 </div>
               </div>
               <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-                <nav class="navbar navbar-expand-lg topnav">
-                  <div class="container-fluid">
-                    <button
-                      class="navbar-toggler"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#navbarNavAltMarkup"
-                      aria-controls="navbarNavAltMarkup"
-                      aria-expanded="false"
-                      aria-label="Toggle navigation"
-                    >
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div
-                      class="collapse navbar-collapse"
-                      id="navbarNavAltMarkup"
-                    >
-                      <div class="navbar-nav">
-                        <a class="nav-link" aria-current="page" href="#">
-                          Home
+                <nav class="navbar navbar-expand-lg navbar topnav">
+                  <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarTogglerDemo01"
+                    aria-controls="navbarTogglerDemo01"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div
+                    class="collapse navbar-collapse"
+                    id="navbarTogglerDemo01"  >
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                      <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                          Home <span class="sr-only">(current)</span>
                         </a>
-                        <a class="nav-link" aria-current="page" href="#">
-                          Consulting
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        Consulting
                         </a>
-                        <a class="nav-link" aria-current="page" href="#">
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
                           Training
                         </a>
-                        <a class="nav-link" aria-current="page" href="#">
-                          Examinations
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                          Examination
                         </a>
-                        <a class="nav-link" aria-current="page" href="#">
-                          Knowledge
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        Knowledge Center
                         </a>
-                        <a class="nav-link" aria-current="page" href="#">
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
                           Clients
                         </a>
-                        <a class="nav-link" aria-current="page" href="#">
-                          Company
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                          Clients
                         </a>
-                      </div>
-                      <div>
-                        <button type="button" class="button1">
+                      </li>
+                      <li class="nav-item">
+                      <button type="button" class="button1">
                           Register
                         </button>
-                      </div>
-                    </div>
+                      </li>
+                    </ul>
                   </div>
                 </nav>
+                
+
                 <div class="row headingban">
                   <div class="col-md">
-                  <h5>NEXT COURSE STARTS FROM 1 DEC 2021</h5>
-                  <h1>Professional Scrum Developer</h1>
-                </div>
+                    <h5>NEXT COURSE STARTS FROM 1 DEC 2021</h5>
+                    <h1>Professional Scrum Developer</h1>
+                  </div>
                 </div>
                 <div class="row headingmob">
-                  <div class="col-md" className="textover"> 
-                  <h6>Next Course Starts from 21st Dec</h6>
-                  <h6>PROFESSIONAL SCRUM DEVELOPER</h6>
-                </div>
+                  <div class="col-md" className="textover">
+                    <h6>Next Course Starts from 21st Dec</h6>
+                    <h6>PROFESSIONAL SCRUM DEVELOPER</h6>
+                  </div>
                 </div>
               </div>
             </div>
